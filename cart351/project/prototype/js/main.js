68cc1c9bb1fc0b50c3afd5cac76bc1461b540875
MaFan8 <maple_f@hotmail.com>
2021-11-23 00:21:29 -0500

    used gridNXZ instead of mesh

--- cart351/project/prototype/js/main.js
+++ cart351/project/prototype/js/main.js
@@ -2,29 +2,46 @@ let scene, camera, renderer, cube, sphere;
 let meshFloor;
 
 let keyboard = {};
-let player = {
-  height: 2,
+let viewer = {
+  height: 2.5,
   speed: 0.2,
   turnSpeed: Math.PI * 0.02
 };
 let USE_WIREFRAME = true;
 
+init();
+animate();
+
 function init() {
+
+  let width = window.innerWidth;
+	let height = window.innerHeight;
+
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+
+  camera.position.y = 160;
+	camera.position.z = 500;
+
+  camera.position.set(0, viewer.height, -8);
+  camera.lookAt (new THREE.Vector3(0,0,0));
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize (width, height);
+	document.body.appendChild (renderer.domElement)
 
   // create floor
-  meshFloor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10, 10, 10),
-    new THREE.MeshBasicMaterial({
-      color: 0xb6c1db,
-      wireframe: USE_WIREFRAME,
-      transparent: true,
-      opacity: 0.25
-    })
-  );
-  meshFloor.rotation.x -= Math.PI / 2; // Rotate the floor 85 degrees
-  scene.add(meshFloor);
+  // meshFloor = new THREE.Mesh(
+  //   new THREE.PlaneGeometry(10, 10, 10, 10),
+  //   new THREE.MeshBasicMaterial({
+  //     color: 0xb6c1db,
+  //     wireframe: USE_WIREFRAME,
+  //     transparent: true,
+  //     opacity: 0.25
+  //   })
+  // );
+  // meshFloor.rotation.x -= Math.PI / 2; // Rotate the floor 85 degrees
+  // scene.add(meshFloor);
 
   // create materials
   cube = new THREE.Mesh(
@@ -38,67 +55,44 @@ function init() {
   scene.add(cube);
 
   sphere = new THREE.Mesh(
-    new THREE.SphereGeometry( .5, 10, 8),
+    new THREE.SphereGeometry(.5, 10, 8),
     new THREE.MeshBasicMaterial({
       color: 0x07374a,
     })
   );
-  sphere.position.set(2,1,-4);
+  sphere.position.set(2, 1, -4);
   scene.add(sphere);
 
-  camera.position.set(0, player.height, -8);
-  camera.lookAt(new THREE.Vector3(0, player.height, 0));
-
-  renderer = new THREE.WebGLRenderer({
-    antialias: true
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
   // udate canvas when resizing window
-  window.addEventListener('resize', function() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-  });
+  // window.addEventListener('resize', function() {
+  //   // let width = window.innerWidth;
+  //   // let height = window.innerHeight;
+  //   renderer.setSize(width, height);
+  //   camera.aspect = width / height;
+  //   camera.updateProjectionMatrix();
+  // });
 
-  animate();
-}
 
+  let gridXZ = new THREE.GridHelper(100, 10);
+	// gridXZ.setRGB( new THREE.Color("rgb(143,143,143)"), new THREE.Color("rgb(128,128,128)"));
+	scene.add(gridXZ);
+
+	let pointLight = new THREE.PointLight ( 0x0000ff);
+	pointLight.position.set (0,300,200);
+	scene.add (pointLight);
+
+	let ambientLight = new THREE.AmbientLight (0x808080 );
+	scene.add(ambientLight);
+} // END INIT
+
+
+// ANIMATE
 function animate() {
   requestAnimationFrame(animate);
-
-  // mesh.rotation.x += 0.01;
-  // mesh.rotation.y += 0.02;
-
-  // // Keyboard movement inputs
-  // if(keyboard[87]){ // W key
-  // 	camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-  // 	camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
-  // }
-  // if(keyboard[83]){ // S key
-  // 	camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-  // 	camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
-  // }
-  // if(keyboard[65]){ // A key
-  // 	// Redirect motion by 90 degrees
-  // 	camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
-  // 	camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
-  // }
-  // if(keyboard[68]){ // D key
-  // 	camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
-  // 	camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
-  // }
-  //
-  // // Keyboard turn inputs
-  // if(keyboard[37]){ // left arrow key
-  // 	camera.rotation.y -= player.turnSpeed;
-  // }
-  // if(keyboard[39]){ // right arrow key
-  // 	camera.rotation.y += player.turnSpeed;
-  // }
 
   let xSpeed = 0.0005;
   let ySpeed = 0.0005;
@@ -106,7 +100,7 @@ function animate() {
   document.addEventListener("keydown", onDocumentKeyDown, false);
 
   function onDocumentKeyDown(event) {
-    var keyCode = event.which;
+    let keyCode = event.which;
     if (keyCode == 38) {
       cube.position.z += ySpeed;
     } else if (keyCode == 40) {
@@ -120,6 +114,22 @@ function animate() {
     }
   };
 
+  // if (cube != undefined) {
+  //   cube.scale.set(0.2, 0.2, 0.2);
+  //   cube.position.set(pos.x, pos.y, pos.z);
+  //   cuber.rotation.y = (angle + Math.PI / 2);
+  //
+  //   var relativeCameraOffset = new THREE.Vector3(0, 150, -250);
+  //
+  //   //var cameraOffset = relativeCameraOffset.applyMatrix4( toycar.matrixWorld );
+  //
+  //   // this is more elegant solution
+  //
+  //   var cameraOffset = cube.localToWorld(relativeCameraOffset);
+  //
+  //   camera.position.copy(cameraOffset);
+  //   camera.lookAt(cube.position);
+  // }
 
   renderer.render(scene, camera);
 }
