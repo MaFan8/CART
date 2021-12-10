1cbd5ef73aba9169f293cfc737d26ea107888a73
MaFan8 <maple_f@hotmail.com>
2021-12-09 21:52:50 -0500

    tried to make cube and camera move as one

--- cart351/project/prototype/js/main.js
+++ cart351/project/prototype/js/main.js
@@ -8,9 +8,9 @@ let viewer = {
   speed: 0.2,
   turnSpeed: Math.PI * 0.02,
   angle: 0,
-  position: new THREE.Vector3(0,0,0)
+  pos: new THREE.Vector3(0, 0, 0)
 };
-
+let player;
 let USE_WIREFRAME = true;
 
 init();
@@ -19,20 +19,22 @@ animate();
 function init() {
 
   let width = window.innerWidth;
-	let height = window.innerHeight;
+  let height = window.innerHeight;
 
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
 
   camera.position.y = 160;
-	camera.position.z = 500;
+  camera.position.z = 500;
 
   camera.position.set(0, viewer.height, -8);
-  camera.lookAt (new THREE.Vector3(0,0,0));
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.setSize (width, height);
-	document.body.appendChild (renderer.domElement)
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(width, height);
+  document.body.appendChild(renderer.domElement)
 
   // create floor
   // meshFloor = new THREE.Mesh(
@@ -67,7 +69,9 @@ function init() {
   sphere.position.set(2, 1, -4);
   scene.add(sphere);
 
-  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
   renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
@@ -82,15 +86,15 @@ function init() {
 
 
   let gridXZ = new THREE.GridHelper(100, 10);
-	// gridXZ.setRGB( new THREE.Color("rgb(143,143,143)"), new THREE.Color("rgb(128,128,128)"));
-	scene.add(gridXZ);
+  // gridXZ.setRGB( new THREE.Color("rgb(143,143,143)"), new THREE.Color("rgb(128,128,128)"));
+  scene.add(gridXZ);
 
-	let pointLight = new THREE.PointLight ( 0x0000ff);
-	pointLight.position.set (0,300,200);
-	scene.add (pointLight);
+  let pointLight = new THREE.PointLight(0x0000ff);
+  pointLight.position.set(0, 300, 200);
+  scene.add(pointLight);
 
-	let ambientLight = new THREE.AmbientLight (0x808080 );
-	scene.add(ambientLight);
+  let ambientLight = new THREE.AmbientLight(0x808080);
+  scene.add(ambientLight);
 } // END INIT
 
 
@@ -99,53 +103,62 @@ function animate() {
 
   delta = clock.getDelta();
 
-  let dir = new THREE.Vector3(1,0,0);
-  dir.multiplyScalar (delta*viewer.speed);	//dir *= dt*speed;
-	dir.applyAxisAngle (new THREE.Vector3(0,1,0), viewer.angle);
-	viewer.position.add (dir); 	//pos = pos + dir;
+  let dir = new THREE.Vector3(1, 0, 0);
+  dir.multiplyScalar(delta * cube.speed); //dir *= dt*speed;
+  dir.applyAxisAngle(new THREE.Vector3(0, 1, 0), viewer.angle);
+  viewer.pos.add(dir); //pos = pos + dir;
 
-  
+  if (player != undefined) {
+    player.scale.set(0.2, 0.2, 0.2);
+    player.position.set(viewer.pos.x, viewer.pos.y, viewer.pos.z);
+    player.rotation.y = (viewer.angle + Math.PI / 2);
 
+    let relativeCameraOffset = new THREE.Vector3(0, 150, -250);
 
-  // requestAnimationFrame(animate);
-  //
-  // let xSpeed = 0.0005;
-  // let ySpeed = 0.0005;
-  //
-  // document.addEventListener("keydown", onDocumentKeyDown, false);
-  //
-  // function onDocumentKeyDown(event) {
-  //   let keyCode = event.which;
-  //   if (keyCode == 38) {
-  //     cube.position.z += ySpeed;
-  //   } else if (keyCode == 40) {
-  //     cube.position.z -= ySpeed;
-  //   } else if (keyCode == 39) {
-  //     cube.position.x -= xSpeed;
-  //   } else if (keyCode == 37) {
-  //     cube.position.x += xSpeed;
-  //   } else if (keyCode == 8) {
-  //     cube.position.set(0, 0, 0);
-  //   }
-  // };
+    let cameraOffset = relativeCameraOffset.applyMatrix4(player.matrixWorld);
 
-  // if (cube != undefined) {
-  //   cube.scale.set(0.2, 0.2, 0.2);
-  //   cube.position.set(pos.x, pos.y, pos.z);
-  //   cuber.rotation.y = (angle + Math.PI / 2);
-  //
-  //   var relativeCameraOffset = new THREE.Vector3(0, 150, -250);
-  //
-  //   //var cameraOffset = relativeCameraOffset.applyMatrix4( toycar.matrixWorld );
-  //
-  //   // this is more elegant solution
-  //
-  //   var cameraOffset = cube.localToWorld(relativeCameraOffset);
-  //
-  //   camera.position.copy(cameraOffset);
-  //   camera.lookAt(cube.position);
-  // }
+    camera.position.x = cameraOffset.x;
+    camera.position.y = cameraOffset.y;
+    camera.position.z = cameraOffset.z;
+    camera.lookAt(player.position);
+  }
 
+  requestAnimationFrame(animate);
+  // update();
+  render();
+}
+
+function myclamp(x, lo, hi) {
+  if (x < lo) return lo;
+  if (x > hi) return hi;
+  return x;
+}
+
+function requestAnimationFrame(animate) {
+  document.addEventListener("keydown", onDocumentKeyDown, false);
+  let xSpeed = 0.0005;
+  let ySpeed = 0.0005;
+
+  function onDocumentKeyDown(event) {
+    let keyCode = event.which;
+    if (keyCode == 38) {
+      // viewer.angle += 0.01;
+          cube.position.z += ySpeed;
+
+    } else if (keyCode == 40) {
+      viewer.angle -= 0.01;
+    } else if (keyCode == 39) {
+      viewer.speed += 0.5;
+    } else if (keyCode == 37) {
+      viewer.speed -= 0.5;
+    } else if (keyCode == 8) {
+      viewer.pos.set(0, 0, 0);
+    }
+    viewer.speed = myclamp(viewer.speed, 0.0, 20.0);
+  }
+}
+
+function render() {
   renderer.render(scene, camera);
 }
 
@@ -160,4 +173,4 @@ function keyUp(event) {
 window.addEventListener('keydown', keyDown);
 window.addEventListener('keyup', keyUp);
 
-window.onload = init;
+// window.onload = init;
