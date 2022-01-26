1a87d2f9e082760d40af157b3780e863283c0fa6
MaFan8 <maple_f@hotmail.com>
2022-01-25 22:04:57 -0500

    Review on different ways to retrieve results

--- cart351/project/prototype2/public_html/viewResultsRef.php
+++ cart351/project/prototype2/public_html/viewResultsRef.php
@@ -11,10 +11,30 @@
 try {
  require('openDB.php');
 
- // retrieving results
- $sql_select='SELECT * FROM Collection';
+ // retrieving results examples: (**ALWAYS READ FROM RIGHT TO LEFT**)
+ // $sql_select='SELECT * FROM Collection'; // from whole table
+ // $sql_select='SELECT * FROM Collection WHERE pieceID = 1'; // from spec. ID
+ // $sql_2="SELECT * FROM Collection WHERE creationDate >=Date('2002-01-01')";
+ // $sql_2="SELECT * FROM Collection WHERE creationDate >=Date('2002-01-01') AND artist = 'Sarah'"; // AND
+ // $sql_2="SELECT * FROM Collection WHERE geoLoc = 'Montreal' OR geoLoc = 'London'";
+ // $sql_2="SELECT * FROM Collection WHERE (creationDate >=Date('2003-01-01') AND artist = 'Sarah')OR(creationDate <=Date('2000-01-01') AND artist = 'Stephen')";
+
+  // $sql_selectA = "SELECT pieceID, title, creationDate FROM Collection";
+  // $sql_selectA = "SELECT artist FROM Collection";
+  // $sql_selectA = "SELECT DISTINCT artist FROM Collection"; // only unique artist names
+  // $sql_selectA = "SELECT creationDate, artist FROM Collection WHERE artist = 'Harold' OR artist = 'Sarah'"; // only date + artist where its sarah/harold
+  // $sql_selectA = "SELECT creationDate, artist FROM Collection WHERE artist = 'Harold' OR artist = 'Sarah' ORDER BY creationDate"; // ORDER BY
+
+  // $sql_countA = "SELECT COUNT(*) FROM Collection";
+  // $sql_countA = "SELECT COUNT(*) FROM Collection WHERE creationDate >=Date('2000-01-01')"; // combine WHERE with counting
+  // $sql_countA = "SELECT artist, COUNT(*) FROM Collection GROUP BY artist"; // count GROUP BY artists
+  // $sql_countA = "SELECT geoLoc, COUNT(*) FROM Collection GROUP BY geoLoc";
+  // $sql_countA = "SELECT artist, geoLoc, COUNT(*) FROM Collection GROUP BY artist,geoLoc"; // give number where artist and location are the same
+  $sql_countA = "SELECT artist, COUNT(*) FROM Collection WHERE artist ='Sarah' OR artist ='Harold' GROUP BY artist"; // **Read WHERE before GROUP BY**
+
+
  // the result set
- $result = $file_db->query($sql_select);
+ $result = $file_db->query($sql_countA);
  if (!$result) die("Cannot execute query.");
 
  // // fetch first row ONLY...
@@ -45,8 +65,8 @@ try {
   // put image in last
     echo "</div>";
     // access by key
-    $imagePath = $row["image"];
-    echo "<img src = $imagePath \>";
+    // $imagePath = $row["image"];
+    // echo "<img src = $imagePath \>";
     echo "</div>";
 }//end while
 echo"</div>";
