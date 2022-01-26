cfd30e859e65c3ab78d60a498c4e334b314c3622
MaFan8 <maple_f@hotmail.com>
2022-01-25 19:22:51 -0500

    Create table COLLECTION in database

--- cart351/project/prototype2/public_html/setUp_DB.php
+++ cart351/project/prototype2/public_html/setUp_DB.php
@@ -1,3 +1,17 @@
 <?php
  require('openDB.php');
+
+ try{
+$theQuery = "CREATE TABLE Collection (pieceID INTEGER PRIMARY KEY NOT NULL, artist TEXT, title TEXT,geoLoc TEXT, creationDate TEXT,descript ,image TEXT)";
+// INTEGER PRIMARY KEY NOT NULL = pieceID has to be unique
+$file_db ->exec($theQuery); //'exec' when we are changing database
+// file_db = database object -> execute the table
+echo ("Collection table created successfully<br>");
+$file_db = null; // simulating closing database
+}
+catch(PDOException $e) {
+    // Print PDOException message
+    echo $e->getMessage();
+  }
+
 ?>
