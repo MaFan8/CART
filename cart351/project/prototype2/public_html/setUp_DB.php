99537b32097f7b96118a68281b98364434a07935
MaFan8 <maple_f@hotmail.com>
2022-02-12 00:49:28 -0500

    Deleted old table and data, restart (added timestamp)

--- cart351/project/prototype2/public_html/setUp_DB.php
+++ cart351/project/prototype2/public_html/setUp_DB.php
@@ -3,8 +3,17 @@
 
  try{
    // Create Table
-   $theQuery = "CREATE TABLE Collection (pieceID INTEGER PRIMARY KEY NOT NULL, artist TEXT, email TEXT, title TEXT, work TEXT, file TEXT)";
-   // INTEGER PRIMARY KEY NOT NULL = pieceID has to be unique
+   $theQuery = "CREATE TABLE Collection (
+     entryID int NOT NULL,
+     Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+     artist TEXT,
+     email TEXT,
+     title TEXT,
+     work TEXT,
+     file TEXT,
+     pieceID INTEGER PRIMARY KEY NOT NULL
+   )";
+
    $file_db ->exec($theQuery); //'exec' when we are changing database
    // file_db = database object -> execute the table
    echo ("Collection table created successfully<br>");
